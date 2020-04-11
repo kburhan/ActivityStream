0126205682d85a4a47f7065af3eb0bfadbcb0ea7
SCybertek <snuryyeva@gmail.com>
2020-04-11 00:30:23 -0400

    added abstract base page

---
+++ src/test/java/com/bitrix24/pages/AbstractBasePage.java
@@ -0,0 +1,37 @@
+package com.bitrix24.pages;
+
+
+import com.bitrix24.utilities.BrowserUtilities;
+import com.bitrix24.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+/**
+ * This abstract class will extent to every PageClass
+ * Here we store common WebElements
+ */
+public abstract class AbstractBasePage {
+
+    protected WebDriver driver = Driver.getDriver();
+    protected WebDriverWait wait = new WebDriverWait(driver, 15);
+
+    public AbstractBasePage() {
+        PageFactory.initElements(driver, this);
+    }
+
+
+    public void navigateTo(String tabName) {
+
+        String tabNameXpath = "//span[text()='" + tabName + "']";
+        WebElement tabElement = driver.findElement(By.xpath(tabNameXpath));
+        wait.until(ExpectedConditions.elementToBeClickable(tabElement));
+        tabElement.click();
+        BrowserUtilities.wait(3);
+    }
+
+}
---
+++ src/test/java/com/bitrix24/pages/LoginPage.java
@@ -0,0 +1,52 @@
+package com.bitrix24.pages;
+
+import com.bitrix24.utilities.BrowserUtilities;
+import com.bitrix24.utilities.ConfigurationReader;
+import com.bitrix24.utilities.Driver;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage extends AbstractBasePage{
+
+
+    @FindBy(css = "input[type='text']")
+    private WebElement username;
+
+    @FindBy(css = "input[type='password']")
+    private WebElement password;
+
+    @FindBy(css = "input[type='submit']")
+    private WebElement login;
+
+    @FindBy(linkText = "Forgot your password?")
+    private WebElement forgatPassword;
+
+    @FindBy(css = "[class='errortext']")
+    private WebElement warningMessage;
+
+
+    public LoginPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+
+    public String getWarningMessageText(){
+        return warningMessage.getText();
+    }
+
+    public void login(String usernameValue, String passwordValue) {
+        this.username.sendKeys(usernameValue);
+        this.password.sendKeys(passwordValue, Keys.ENTER);
+        BrowserUtilities.waitForPageToLoad(10);
+        BrowserUtilities.wait(3);
+
+    }
+    public void login(){
+        this.username.sendKeys(ConfigurationReader.getProperty("username1"));
+        this.password.sendKeys(ConfigurationReader.getProperty("password"), Keys.ENTER);
+        BrowserUtilities.waitForPageToLoad(10);
+        BrowserUtilities.wait(3);
+    }
+}
