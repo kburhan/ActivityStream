68230a623d7ca500555e25953b68dc1d6f542a90
SCybertek <snuryyeva@gmail.com>
2020-04-12 02:05:27 -0400

    renamed a class

--- src/test/java/com/bitrix24/tests/SendingMessagesTests.java
+++ src/test/java/com/bitrix24/tests/SendingMessagesTests.java
@@ -6,7 +6,7 @@ import com.bitrix24.utilities.BrowserUtilities;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class UserStory1 extends AbstractTestBase {
+public class SendingMessagesTests extends AbstractTestBase {
 
     /**
      * 1. As a user, I should be able to send messages by clicking on Message tab under Active Stream.
