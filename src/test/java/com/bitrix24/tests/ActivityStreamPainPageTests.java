e855d61d69bd883e34947fa6171b3bab6fb184ed
zeeyoda <ziyoda2000@yahoo.com>
2020-04-16 17:01:23 -0400

    added ActivityStreamMainPageAndTestPage

---
+++ src/test/java/com/bitrix24/tests/ActivityStreamPainPageTests.java
@@ -0,0 +1,22 @@
+package com.bitrix24.tests;
+
+import com.bitrix24.pages.ActivityStream_MainPage;
+import com.bitrix24.pages.LoginPage;
+import org.testng.annotations.Test;
+
+
+public class ActivityStreamPainPageTests extends AbstractTestBase{
+    LoginPage loginPage = new LoginPage();
+    ActivityStream_MainPage activityStream_mainPage = new ActivityStream_MainPage();
+
+     @Test
+    public void clickOnAddToFavorites(){
+        loginPage.login();
+        activityStream_mainPage.clickOnAddToFavoritesStarIcon();
+    }
+    @Test
+    public void clickOnComment(){
+         loginPage.login();
+         activityStream_mainPage.clickOnComment();
+    }
+}
